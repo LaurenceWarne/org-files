f522ccd25897d95a3e2987a3e1063238c1f9d0aa
lWarne <laurencewarne@gmail.com>
2021-09-23 10:06:28 +0100

    Add GH actions file

---
+++ ox-yaow-publish.el
@@ -0,0 +1,56 @@
+;; Add melpa repository
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)  ; Get most recent versions of org mode
+
+(package-initialize)
+
+;; Check if use-package is installed and install if it's not
+;; Note it's a melpa package so this has to come after the melpa repository is added
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (eval-when-compile (require 'use-package)))
+
+(use-package htmlize)
+
+(use-package ox-yaow
+  :config
+  ;; Stolen from https://github.com/fniessen/org-html-themes
+  (setq rto-css '("https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css"
+                  "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css")
+        rto-js '("https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js"
+                 "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js")
+        extra-js '("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
+                   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" )
+        ox-yaow-html-head (concat (mapconcat (lambda (url) (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" url "\"/>\n")) rto-css "")
+                                  (mapconcat (lambda (url) (concat "<script src=\"" url "\"></script>\n")) (append rto-js extra-js) ""))
+        org-publish-project-alist (cons
+                                   `("wiki"
+                                     :base-directory "~/org/"
+                                     :base-extension "org"
+                                     :publishing-directory "~/wiki/"
+                                     :html-head ,ox-yaow-html-head
+                                     :html-preamble t
+                                     :recursive t
+                                     :exlude ".*steam\.org"
+                                     :publishing-function ox-yaow-publish-to-html
+                                     :preparation-function ox-yaow-preparation-fn
+                                     :completion-function ox-yaow-completion-fn
+                                     :ox-yaow-wiki-home-file "~/org/wiki.org"
+
+                                     :ox-yaow-file-blacklist ("~/org/maths/answers.org")
+                                     :ox-yaow-depth 2)
+                                   org-publish-project-alist)))
+
+(message "Running publish...")
+(org-publish-project "wiki")
