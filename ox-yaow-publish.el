9c3e93c4be9af97af3f357f19b053b01bab0584c
lWarne <laurencewarne@gmail.com>
2022-01-04 08:29:44 +0000

    Fix ox-yaow-publish.el

--- ox-yaow-publish.el
+++ ox-yaow-publish.el
@@ -1,5 +1,4 @@
 (require 'package)
-(require 'org)
 
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -24,20 +23,23 @@
 
 (setq use-package-always-ensure t)
 
-(use-package dash)
+(use-package dash
+  :demand t)
 
 (use-package org
   :after dash
-  :ensure org-plus-contrib
+  :demand t
   :config
   (setq org-confirm-babel-evaluate nil)
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((python . t)
      (shell . t)
-     (emacs-lisp . t)))
-  (add-to-list 'org-src-lang-modes '("python" . python-no-elpy))
-  :pin org)
+     (emacs-lisp . t))))
+
+(use-package org-contrib
+  :after org
+  :demand t)
 
 (use-package scala-mode
   :mode "\\.s\\(c\\|cala\\|bt\\)$")
@@ -46,46 +48,43 @@
 
 (use-package ox-yaow
   :after org
+  :demand t
   :config
   ;; Stolen from https://github.com/fniessen/org-html-themes
-  (setq rto-css '("https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css"
-                  "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css")
-        rto-js '("https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js"
-                 "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js")
-        extra-js '("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
-                   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" )
-        ox-yaow-html-head
-        (concat
-         (mapconcat
-          (lambda (url)
-            (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\""
-                    url
-                    "\"/>\n"))
-          rto-css
-          "")
-         (mapconcat
-          (lambda (url)
-            (concat "<script src=\"" url "\"></script>\n"))
-          (append rto-js extra-js) "")
-         "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://raw.githubusercontent.com/LaurenceWarne/org-files/master/code.css\" />")
-        org-publish-project-alist (cons
-                                   `("wiki"
-                                     :base-directory "/org/"
-                                     :base-extension "org"
-                                     :publishing-directory "/wiki/"
-                                     :html-head ,ox-yaow-html-head
-                                     :html-preamble t
-                                     :htmlized-source t
-                                     :recursive t
-                                     :exlude ".*steam\.org"
-                                     :publishing-function ox-yaow-publish-to-html
-                                     :preparation-function ox-yaow-preparation-fn
-                                     :completion-function ox-yaow-completion-fn
-                                     :ox-yaow-wiki-home-file "/org/wiki.org"
-
-                                     :ox-yaow-file-blacklist ("org/maths/answers.org")
-                                     :ox-yaow-depth 2)
-                                   org-publish-project-alist)))
+  (let* ((rto-css '("https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css"
+                    "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css"))
+         (rto-js '("https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js"
+                   "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js"))
+         (extra-js '("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
+                     "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" ))
+         (ox-yaow-html-head (concat (mapconcat (lambda (url) (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" url "\"/>\n")) rto-css "")
+                                    (mapconcat (lambda (url) (concat "<script src=\"" url "\"></script>\n")) (append rto-js extra-js) ""))))
+    (setq org-publish-project-alist
+          (append
+           `(("wiki-pages"
+              :base-directory "~/org/"
+              :base-extension "org"
+              :publishing-directory "~/wiki/"
+              :html-head ,ox-yaow-html-head
+              :html-preamble t
+              :recursive t
+              :exclude ".*steam.*"
+              :publishing-function ox-yaow-publish-to-html
+              :preparation-function ox-yaow-preparation-fn
+              :completion-function ox-yaow-completion-fn
+              :ox-yaow-wiki-home-file "~/org/wiki.org"
+              :ox-yaow-file-blacklist ("~/org/maths/answers.org")
+              :ox-yaow-depth 2)
+             ("wiki-static"
+              :base-directory "~/org/"
+              :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+              :publishing-directory "~/wiki/"
+              :recursive t
+              :exclude ,(rx (0+ ".") (or "steam" "ltximg") (0+ "."))
+              :publishing-function org-publish-attachment)
+             ("wiki"
+              :components ("wiki-pages" "wiki-static")))
+           org-publish-project-alist))))
 
 (require 'org)
 (require 'scala-mode)
